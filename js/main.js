4821a274b8055e4d42e8af883339c2d9e985797e
Sofian Mourabit <smourabit@informationmapping.com>
2017-03-23 18:27:01 +0700

    added 2 more charts types to page

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 // Line Chart
 
-const ctx = document.getElementById("traffic");
-let data = {
+const trafficCanvas = document.getElementById("traffic");
+const dailyTrafficCanvas = document.getElementById("dailyTraffic");
+const mobileUsersCanvas = document.getElementById("mobileUsers");
+let dataTraffic = {
   labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
   datasets: [
     {
       fill: true,
       lineTension: 0.1,
-      backgroundColor: "rgba(212,117,0,0.4)",
-      borderColor: "rgba(212,117,0,1)",
+      backgroundColor: "rgba(231,110,60,0.4)",
+      borderColor: "rgba(231,110,60,1)",
       pointBorderColor: "rgba(204,128,61,1)",
       pointBackgroundColor: "#fff",
       pointBorderWidth: 1,
@@ -19,12 +21,43 @@ let data = {
       pointRadius: 5,
       pointHitRadius: 10,
       pointStyle: 'circle',
-      data: [0,750, 1250, 1000, 1500, 2000, 1500, 1750, 1250, 1750, 2000, 1750, 2000]
+      data: [0,500, 1000, 750, 1250, 1750, 1250, 1500, 1000, 1500, 2000, 1500, 2000]
+    }
+  ]
+};
+let dataDailyTraffic = {
+  labels: ["S", "M", "T", "W", "T", "F", "S"],
+  datasets: [
+    {
+      fill: true,
+      borderWidth: 2,
+      backgroundColor: "rgba(194, 213, 216,0.4)",
+      borderColor: "rgba(194, 213, 216,1)",
+      data: [50, 75, 150, 100, 200, 175, 75]
+    }
+  ]
+};
+let dataMobileUsers = {
+  labels: ["Phones", "Tablets", "Desktop"],
+  datasets: [
+    {
+      data: [23, 26, 76],
+      backgroundColor: [
+        "#d47500",
+        "#c2d5d8",
+        "#2A3D45"
+      ],
+      hoverBackgroundColor: [
+        "#d47500",
+        "#c2d5d8",
+        "#2A3D45"
+      ]
     }
   ]
 };
 
-const options = {
+const lineOptions = {
+  responsive: true,
   legend: {
     display: false
   },
@@ -38,16 +71,50 @@ const options = {
       }
     }],
     yAxes: [{
-      display: true,
       ticks: {
         beginAtZero: true,
         max: 2500
+      },
+    }]
+  }
+};
+const barOptions = {
+  responsive: true,
+  legend: {
+    display: false
+  },
+  tooltips: {
+    enabled: false
+  },
+  scales: {
+    yAxes: [{
+      gridLines: { offsetGridLines: true },
+      ticks: {
+        beginAtZero: true,
+        max: 250
       }
     }]
   }
 };
-let traffic = new Chart(ctx, {
+const doughnutOptions = {
+  responsive: true,
+  tooltips: {
+    enabled: false
+  },
+};
+
+let traffic = new Chart(trafficCanvas, {
   type: 'line',
-  data: data,
-  options: options
+  data: dataTraffic,
+  options: lineOptions
+});
+let dailyTraffic = new Chart(dailyTrafficCanvas, {
+  type: 'bar',
+  data: dataDailyTraffic,
+  options: barOptions
+});
+let mobileUsers = new Chart(mobileUsersCanvas, {
+  type: 'doughnut',
+  data: dataMobileUsers,
+  options: doughnutOptions
 });
